38c17a1cededb76335fe5ccd8b19eaf4e77a8bf8
Piyush <developerpiyush1102k2@gmail.com>
2023-12-07 00:37:08 +0530

    Added RegisterUserValidator

---
+++ TennisCourtBookings.Application/Features/UserFeatures/RegisterUser/RegisterUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisCourtBookings.Application.Features.UserFeatures.UpdateUser;
+
+namespace TennisCourtBookings.Application.Features.UserFeatures.RegisterUser
+{
+    public sealed class UpdateUserValidator : AbstractValidator<RegisterUserRequest>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().MaximumLength(50).EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(3).MaximumLength(20);
+        }
+    }
+}
